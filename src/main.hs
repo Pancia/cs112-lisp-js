01267d0634165a478e9f187dd29b8278e800da48
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-05 19:31:09 -0800

    Added -t outputType, which determines what to compile down to.

--- src/main.hs
+++ src/main.hs
@@ -13,7 +13,8 @@ import Control.Monad.Except (throwError)
 
 import Data.Char (toLower)
 
-import qualified LispJs as L
+import qualified LispJs as JS
+import qualified LispPy as PY
 import qualified Utils as U
 import qualified Parser as P
 
@@ -30,13 +31,13 @@ data Options = Options  {
 defaultOptions :: Options
 defaultOptions = Options {
     optInput  = "in.jsp",
-    optOutput = "out.jsp",
+    optOutput = "out",
     optLisp = "",
     optType = JS}
 
 options :: [OptDescr (Options -> Options)]
 options = [Option "i" ["input"]     (ReqArg readInput "FILE")     "input file"
-          ,Option "o" ["output"]    (ReqArg readOutput "FILE")    "output file"
+          ,Option "o" ["output"]    (ReqArg readOutput "FILE")    "output filename"
           ,Option "l" ["lisp-expr"] (ReqArg readLispExpr "stdin") "input lisp s-exprs"
           ,Option "t" ["type"]      (ReqArg readOutputType "type") "output file type"]
 
@@ -59,43 +60,49 @@ readOutput arg opts = opts {optOutput = arg}
 main :: IO ()
 main = do args <- getArgs
           let (actions, nonOpts, msgs) = getOpt RequireOrder options args
-              opts = foldl (flip id) defaultOptions actions
+              opts = foldl (flip ($)) defaultOptions actions
               outType = optType opts
-          print $ "opts: " ++ show opts
-          print $ "nonOpts: " ++ show nonOpts
-          print $ "msgs: " ++ show msgs
+              outFile = if '.' `elem` tail (optOutput opts)
+                            then optOutput opts
+                            else optOutput opts ++ ".jsp." ++ (toLower <$> show outType)
+          putStrLn $ ">>opts: " ++ show opts
+          putStrLn $ ">>nonOpts: " ++ show nonOpts
+          putStrLn $ ">>msgs: " ++ show msgs
+          putStrLn . take 60 $ repeat '#'
 
-          expr <- if null $ optLisp opts
-                      then liftM readExpr . readFile $ optInput opts
-                      else return . readExpr $ optLisp opts
-          let out = optOutput opts
+          lispVals <- if null $ optLisp opts
+                          then liftM readExpr . readFile $ optInput opts
+                          else return . readExpr $ optLisp opts
           --Show lisp after parsing
-          putStrLn . ("lispVal: " ++) . U.catch . liftM show $ expr
+          putStrLn . (">>lispVals:\n" ++) . U.catch . liftM show $ lispVals
           putStrLn . take 60 $ repeat '#'
 
           --Convert to outType, write to out, and print
-          let translated = liftM (traslateTo outType <$>) expr
-          print . ("jsVal: " ++) . show $ translated
+          src <- case outType of
+                     JS -> do let jsVals = U.catch . liftM (JS.translate <$>) $ lispVals
+                              putStrLn $ ">>jsVals:\n" ++ show jsVals
+                              putStrLn . take 60 $ repeat '#'
+                              JS.formatJs $ JS.toJS <$> jsVals
+                     PY -> do let pyVals = U.catch . liftM (PY.translate <$>) $ lispVals
+                              putStrLn $ ">>pyVals:\n" ++ show pyVals
+                              putStrLn . take 60 $ repeat '#'
+                              PY.formatPy $ PY.toPY <$> pyVals
+
+          putStrLn $ ">>" ++ outFile ++ ":\n" ++ src
           putStrLn . take 60 $ repeat '#'
-          js <- L.formatJs . U.catch . liftM (L.toJS <$>) $ translated
-          print . ("js: " ++) $ js
-          putStrLn . take 60 $ repeat '#'
-          writeFile out js
-          putStrLn $ out ++ ": " ++ show js
+          writeFile outFile src
 
-          --Execute js, print its result
-          jsOutput <- execJS out
-          print $ "jsOutput: " ++ jsOutput
-    where
-        traslateTo outType = case outType of
-                                 JS -> L.translate
-                                 PY -> undefined
+          --Execute src, print its result
+          print =<< ("stdout: " ++) <$> execSrc outType outFile
 
-execJS :: String -> IO String
-execJS file = do (_, Just hout, _, _) <- createProcess $ (proc "jsc" [file]) { std_out = CreatePipe }
-                 hGetContents hout
+execSrc :: OutputType -> String -> IO String
+execSrc outType file = do let exe = case outType of
+                                     JS -> "jsc"
+                                     PY -> "python"
+                          (_, Just hout, _, _) <- createProcess $ (proc exe [file]) { std_out = CreatePipe }
+                          hGetContents hout
 
-readExpr :: String -> Either U.CompilerError [L.LispVal]
+readExpr :: String -> Either U.CompilerError [JS.LispVal]
 readExpr input = case parse P.parseExpr "lisp-js" input of
                      Left err -> throwError $ U.ParserErr err
                      Right val -> return val
