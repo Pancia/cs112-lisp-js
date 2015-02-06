5111b06a79c976ca69e3c51e7c9322b8c8dd45c1
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-05 16:16:48 -0800

    Added System.Console.GetOpt usage to main

--- src/main.hs
+++ src/main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Control.Applicative
 import Control.Monad
+
 import System.Environment
 import System.IO
 import System.Process
+import System.Console.GetOpt
 
 import Text.Parsec (parse)
 import Control.Monad.Except (throwError)
@@ -13,15 +15,49 @@ import qualified LispJs as L
 import qualified Utils as U
 import qualified Parser as P
 
+data Options = Options  {
+    optInput  :: String,
+    optOutput :: String,
+    optLisp :: String
+    } deriving (Show)
+
+defaultOptions :: Options
+defaultOptions = Options {
+    optInput  = "in.jsp",
+    optOutput = "out.jsp",
+    optLisp = ""
+    }
+
+options :: [OptDescr (Options -> Options)]
+options = [Option "i" ["input"]     (ReqArg readInput "FILE")     "input file"
+          ,Option "o" ["output"]    (ReqArg readOutput "FILE")    "output file"
+          ,Option "l" ["lisp expr"] (ReqArg readLispExpr "stdin") "input stream"]
+
+readLispExpr :: String -> Options -> Options
+readLispExpr arg opts = opts {optLisp = arg}
+
+readInput :: String -> Options -> Options
+readInput arg opts = opts {optInput = arg}
+
+readOutput :: String -> Options -> Options
+readOutput arg opts = opts {optOutput = arg}
+
 main :: IO ()
 main = do args <- getArgs
-          expr <- if head args == "f"
-                      then liftM readExpr $ readFile (args !! 1)
-                      else return $ readExpr (head args)
-          let out = "out.js"
+          let (actions, nonOpts, msgs) = getOpt RequireOrder options args
+              opts = foldl (\dflt act -> act dflt) defaultOptions actions
+          print $ "opts: " ++ show opts
+          print $ "nonOpts: " ++ show nonOpts
+          print $ "msgs: " ++ show msgs
+
+          expr <- if null $ optLisp opts
+                      then liftM readExpr . readFile $ optInput opts
+                      else return . readExpr $ optLisp opts
+          let out = optOutput opts
           --Show lisp after parsing
           putStrLn . ("lispVal: " ++) . U.catch . liftM show $ expr
           putStrLn . take 60 $ repeat '#'
+
           --Convert to js, write to out, and print
           let jsVals = liftM (L.translate <$>) expr
           print . ("jsVal: " ++) . show $ jsVals
@@ -31,6 +67,7 @@ main = do args <- getArgs
           putStrLn . take 60 $ repeat '#'
           writeFile out js
           putStrLn $ out ++ ": " ++ show js
+
           --Execute js, print its result
           jsOutput <- execJS out
           print $ "jsOutput: " ++ jsOutput
