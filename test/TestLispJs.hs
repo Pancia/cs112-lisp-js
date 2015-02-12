7480a2a001bcb57d4e7d2aea868199f52def6c08
Neeraj Mallampet <nmallamp@ucsc.edu>
2015-02-12 14:11:44 -0800

    updated testfile with new

--- test/TestLispJs.hs
+++ test/TestLispJs.hs
@@ -32,7 +32,8 @@ testReadExpr :: [T.Test]
 testReadExpr = fmap (\(name, (l, r)) -> testCase name (l @=? readExpr' r)) tests
     where
         tests = [("list of numbers", ([L.List [L.Number 3, L.Number 4]], "(3 4)"))
-                ,("list of atoms", ([L.List [L.Atom "foo", L.Atom "bar"]], "(foo bar)"))]
+                ,("list of atoms", ([L.List [L.Atom "foo", L.Atom "bar"]], "(foo bar)"))
+                ,("def obj", ([L.New "Number" [L.Number 5]] , "(new Number 5)"))]
         readExpr' = U.catch . readExpr
 
 testToJS :: [T.Test]
