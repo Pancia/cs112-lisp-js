7bfae042a85494cedd638018f79e2dd289fb1f41
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-25 02:18:06 -0800

    Err, jsp -> loki, damnit!

---
+++ out.loki.js
@@ -0,0 +1,84 @@
+var loki = (function (){
+    var loki = {};
+
+    var sliceArgs = function (args) {
+        return args.length > 0 ? [].slice.call(args, 0) : [];
+    };
+
+    var assertIsFunction = function (f) {
+        if (typeof f !== "function")
+        {throw "loki error: curry expected a function"}
+        return f;
+    };
+
+    var curry = function (fn) {
+        assertIsFunction(fn);
+        return function inner() {
+            var _args = sliceArgs(arguments);
+            if (_args.length === fn.length) {
+                return fn.apply(null, _args);
+            } else if (_args.length > fn.length) {
+                var initial = fn.apply(null, _args);
+                return foldl(fn, initial, _args.slice(fn.length));
+            } else {
+                return function() {
+                    var args = sliceArgs(arguments);
+                    return inner.apply(null, _args.concat(args));
+                };
+            }
+        };
+    };
+
+    var each = curry(function (iterator, items) {
+        assertIsFunction(iterator);
+        if (items == null || !Array.isArray(items)) {return;}
+        items.forEach(function (e, i) {iterator.call(null, e, i)});
+    });
+
+    var foldl = curry(function (iterator, acc, xs) {
+        assertIsFunction(iterator);
+        each(function (x, i) {
+            acc = iterator.call(null, acc, x, i);
+        }, xs);
+        return acc;
+    });
+
+    loki.print = function() {
+        var _log = function(x) {
+            if (typeof console === "object") {console.log(x);}
+            else {assertIsFunction(print)(x);}
+        };
+        var args = sliceArgs(arguments);
+        args.forEach(_log);
+    };
+    loki.get = function(e, i) {return e[i];};
+
+    //Arithmetic
+    loki.plus  = curry(function(x, y) {return x + y});
+    loki.minus = curry(function(x, y) {return x - y});
+    loki.mult  = curry(function(x, y) {return x * y});
+    loki.div   = curry(function(x, y) {return x / y});
+
+    //Logic
+    loki.and = curry(function(x, y) {return x && y});
+    loki.or  = curry(function(x, y) {return x || y});
+    loki.eq  = curry(function(x, y) {return x == y});
+    loki.neq = curry(function(x, y) {return x != y});
+    loki.lt  = curry(function(x, y) {return x < y});
+    loki.lte = curry(function(x, y) {return x <= y});
+    loki.gt  = curry(function(x, y) {return x > y});
+    loki.gte = curry(function(x, y) {return x >= y});
+
+    return loki;
+})();
+//END HELPER FUNCTIONS
+var y = "y";
+var hello_world = function () {
+loki.print("foobar");
+return alert(loki.plus(2, 3, 5))
+};
+var y = "y";
+$("img").click(function () {
+return alert("img")
+});
+loki.print(y);
