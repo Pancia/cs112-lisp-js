b097737099da076ee022101be9883c17b9411b38
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-03-10 13:20:51 -0700

    Added imports fixed new needing space if no args.

--- out.loki.py
+++ out.loki.py
@@ -71,6 +71,14 @@ class Loki:
         return not x
 
 #END LOKI HELPER FUNCTIONS
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.properties import ListProperty
+from kivy.properties import NumericProperty
+from kivy.uix.modalview import ModalView
 class GridEntry(Button):
     def __init__(self):
         self.coords = ListProperty([0, 0])
@@ -96,15 +104,20 @@ class TicTacToeGrid(GridLayout):
                 _on_release = {"on_release" : _button_pressed}
                 (gridEntry.bind(**_on_release) if callable(gridEntry.bind) else gridEntry.bind)
                 (self.add_widget(gridEntry) if callable(self.add_widget) else self.add_widget)
-    def buttonPressed (self, self, button):
+    def buttonPressed (self, button):
         return do(player = {-1 : "0", 1 : "X"}, colors = {-1 : (0,0,0,0), 1 : (0,0,0,0)}, row = Loki.get(button, Loki.get(coords, 0)), column = Loki.get(button, Loki.get(coords, 1)), statusIndex = Loki.plus(Loki.mult(Loki.get(row, 3)), column), alreadyPlayed = Loki.get(self, Loki.get(status, statusIndex)), (t1(self) if Loki.not_(alreadyPlayed) else None))
 
-    def reset (self, self, *args):
+    def reset (self, *args):
         return (lambda  : [self.status = [0, 0, 0, 0, 0, 0, 0, 0, 0], for child in (self.children() if callable(self.children) else self.children):
     child.text = "", self.currentPlayer = 1])()
 
-    def on_status (self, self, instance, newVal):
+    def on_status (self, instance, newVal):
         return do(status = newVal, sums = [Loki.plus(Loki.sc(0, 3)), Loki.plus(Loki.sc(3, 6)), Loki.plus(Loki.sc(6, 9)), Loki.plus(Loki.dc(0, 3)), Loki.plus(Loki.dc(1, 4)), Loki.plus(Loki.dc(2, 3)), Loki.plus(Loki.dc(0, 4)), Loki.plus(Loki.dcm(2, -2, 2))], winner = "", (winner = "Xs win!" if Loki.in_(3, sums) else None), (winner = "Os win!" if Loki.in_(-3, sums) else None), (winner = "Draw!" if !in(0, (self.status() if callable(self.status) else self.status)) else None), ((lambda  : [popup = ModalView(**, {"size_hunt" : [Loki.div(3, 4), Loki.div(1, 2)]}), victoryLabel = Label(**, {"font_size" : 50, "text" : winner}), (popup.add_widget(victoryLabel) if callable(popup.add_widget) else popup.add_widget), (popup.bind(**, {"on_dismiss" : (self.reset() if callable(self.reset) else self.reset)}) if callable(popup.bind) else popup.bind), (popup.open() if callable(popup.open) else popup.open)])() if Loki.neq(winner, "") else None))
+
+
+class TicTacToeApp(app):
+    def build (self, self):
+        return TicTacToeGrid()
 
 
 ((TicTacToeApp().run() if callable(TicTacToeApp().run) else TicTacToeApp().run) if Loki.eq(__name__("__main__")) else None)
